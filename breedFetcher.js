480bd9e5a5bf54b648728416cf431a4faaca81eb
Scarlet Shao <scarletshao@gmail.com>
2021-03-12 00:53:28 +0000

    add cat project

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,12 @@
 const request = require('request');
-const arg = process.argv[2];
-
-request(`http://api.thecatapi.com/v1/breeds/search?q=${arg}`, (error, response,body) => {
-  if (error) {
-    console.log('error:', error);
-  } else {
-    const data = JSON.parse(body);
-    data.length === 0 ? console.log('Didn find the breed name!') : console.log(data);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`http://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error,null);
+    } else {
+      const data = JSON.parse(body);
+      data.length === 0 ? callback('Didn find the breed name!',null) : callback(null,data[0].description);
+    }
+  });
+};
+module.exports = { fetchBreedDescription };
